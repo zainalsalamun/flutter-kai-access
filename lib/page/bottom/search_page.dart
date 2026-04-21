7056e97ace7dc09600daf76b7801fa0592b42d26
zainalsalamun <zainalrtf@gmail.com>
2026-04-21 09:13:33 +0700

    fix(search): use const constructors for improved performance

--- lib/page/bottom/search_page.dart
+++ lib/page/bottom/search_page.dart
@@ -7,11 +7,10 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search Page'),
+        title: const Text('Search Page'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Search Page'),
-
       ),
     );
   }
